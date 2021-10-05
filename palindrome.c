3d382b5896f74a5a9081b7613221bae15778680c
VARSHA K <43096171+varsha2000@users.noreply.github.com>
2021-10-05 18:40:03 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+
+    int n,i,r=0;
+    
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    
+    for(i=n;i!=0;i)
+    {
+        r=r*10;
+        r=r+ i%10;
+        i=i/10;
+    }
+    
+    if(r==n)
+        printf("palindrome");
+    else
+        printf("Not palindrome");
+}
 
